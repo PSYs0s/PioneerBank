b6c290eb140b8781544e6fb969b6f134306d76c5
PSYs0s <asmtohahere@gmail.com>
2021-02-05 03:26:06 +0600

    uploaded

---
+++ scripts/javascript.js
@@ -0,0 +1,37 @@
+//login button event handler
+const loginBtn = document.getElementById("login")
+loginBtn.addEventListener("click",function () {
+    const loginArea = document.getElementById("login-area")
+    loginArea.style.display = "none"
+    const transactionArea = document.getElementById("transaction-area")
+    transactionArea.style.display = "block"
+})
+//deposite button handler
+const depositeBtn = document.getElementById("addDeposit")
+depositeBtn.addEventListener("click",function(){
+    const depositNumber = getInputNumber("depositAmount")
+    updateSpanText("currentDeposit",depositNumber)
+    updateSpanText("currentBalance",depositNumber)
+    document.getElementById("depositAmount").value = ""
+})
+//withdraw button handler
+const withdrawBtn = document.getElementById("addWithdraw")
+withdrawBtn.addEventListener("click",function(){
+    const withdrawNumber = getInputNumber("withdrawAmount")
+    updateSpanText("currentWithdraw",withdrawNumber)
+    updateSpanText("currentBalance",-1 * withdrawNumber)
+    document.getElementById("withdrawAmount").value = ""
+})
+//capture input and transform into number type from user
+function getInputNumber(id){
+    const amount = document.getElementById(id).value
+    const amountNumber = parseFloat(amount)
+    return amountNumber
+}
+//update total balance after deposite
+function updateSpanText(id,addedNumber){
+    const current = document.getElementById(id).innerText
+    const currentNumber = parseFloat(current)
+    const totalAmount = addedNumber + currentNumber
+    document.getElementById(id).innerText = totalAmount
+}
